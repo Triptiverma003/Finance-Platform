28949ca5ce21d1b05aa8525a94e389e9ae85262b
Tripti Verma <vermatripti547@gmail.com>
2025-11-17 14:55:07 +0530

    Fix: Add welth files as regular content (not submodule)

---
+++ welth/app/api/inngest/route.js
@@ -0,0 +1,19 @@
+import { serve } from "inngest/next";
+
+import { inngest } from "@/lib/inngest/clients";
+import {
+  checkBudgetAlerts,
+  generateMonthlyReports,
+  processRecurringTransaction,
+  triggerRecurringTransactions,
+} from "@/lib/inngest/function";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    processRecurringTransaction,
+    triggerRecurringTransactions,
+    generateMonthlyReports,
+    checkBudgetAlerts,
+  ],
+});
